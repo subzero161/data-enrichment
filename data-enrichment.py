7810a4c13d7accd18b1cdb0047867d5de91b921d
subzero161 <58404587+subzero161@users.noreply.github.com>
2022-09-12 22:58:43 -0400

    Update 2

--- data-enrichment.py
+++ data-enrichment.py
@@ -4,3 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #%matplotlib inline
 
+#importing files and setting variables
+
+#SPARCS Data
+sparcs = pd.read_csv('data-enrichment/Data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv')
+
+#neighborhood atlas data (using .txt file)
+nbhood_atlas = pd.read_csv('data-enrichment/Data/US_2019_ADI_Census Block Group_v3.1.txt')
+
+
+#creating columns
+sparcs.columns
+nbhood_atlas.columns
